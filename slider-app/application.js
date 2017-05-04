cdc52e9b379c18afa624af5d4bb14245361ba0e9
Samislx <sami@seluxit.com>
2017-05-04 11:53:42 +0200

    Update application.js

--- slider-app/application.js
+++ slider-app/application.js
@@ -1,4 +1,4 @@
-var value = getValue("RL 1");
+var value = getValue({"type": "switch"});
 var controlState = value.get("state").findWhere({type: "Control"});
 var reportState = value.get("state").findWhere({type: "Report"});
 var number = value.get("number");
@@ -6,8 +6,8 @@ var number = value.get("number");
 $("#slider").roundSlider({
   radius: 85,
   sliderType: "min-range",
-  min: number.min,
-  max: number.max,
+  min: 0,
+  max: 1,
   value: reportState.get("data"),
   change: function(e){
     controlState.save({data: e.value+""});
@@ -17,13 +17,3 @@ $("#slider").roundSlider({
 reportState.on("change:data", function(){
 	$("#slider").roundSlider('setValue', reportState.get("data")+"");
 });
-
-function myFunc(){
-  if(controlState.get("data") == "1"){
-  	controlState.save({data: "0"});
-    $("#slider").roundSlider('setValue', "0");
-  }else{
-    controlState.save({data: "1"});
-    $("#slider").roundSlider('setValue', "1");
-  }
-}
